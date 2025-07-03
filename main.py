4e4674a5cf7a39d9e0ffe13cf39b733bcd24a042
Vasyl <vasyl@example.com>
2025-07-03 21:29:49 +0300

    more dicts

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from pprint import pprint
 import requests
 
-URL = 'https://dummyjson.com/todos'
+URL = "https://dummyjson.com/products"
+params = {
+    "limit": 200,
+    "skip": 0
+}
 
-response = requests.get(url=URL)
+response = requests.get(url=URL, params=params)
+products = response.json()["products"]
 
+total_stock = 0
+total_cost = 0
 
-response_json = response.json()
-pprint(response_json)
+for product in products:
+    # print(product.get("brand"))
+
+    if "brand" in product:
+        if product["brand"] == "Apple":
+            total_stock += product["stock"]
+            total_cost += product["price"] * product["stock"]
+
+print(f"Total stock (всего на складе): {total_stock}")
+print(f"Total total_cost (общая стоимость всех товаров): ${total_cost}")
